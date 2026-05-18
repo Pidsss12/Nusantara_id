cf1b7809931dd43546eda2a7a1c23938027cc7e5
Nizar <nizarcandra0@gmail.com>
2026-05-18 20:01:59 +0700

    modif

--- storage/framework/views/751760b9ec22e1a33c398b85904c9054.php
+++ storage/framework/views/751760b9ec22e1a33c398b85904c9054.php
@@ -188,6 +188,14 @@
                                                         <option value="Inactive" <?php echo e($dest->status == 'Inactive' ? 'selected' : ''); ?>>Inactive</option>
                                                     </select>
                                                 </div>
+                                                <div class="col-md-6">
+                                                    <label class="form-label fw-bold">Location Details</label>
+                                                    <input type="text" name="location" class="form-control" value="<?php echo e($dest->location); ?>" required>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <label class="form-label fw-bold">Quota/Day</label>
+                                                    <input type="number" name="quota_per_day" class="form-control" value="<?php echo e($dest->quota_per_day); ?>">
+                                                </div>
                                                 <div class="col-12">
                                                     <label class="form-label fw-bold">Description</label>
                                                     <textarea name="description" class="form-control" rows="3" required><?php echo e($dest->description); ?></textarea>
